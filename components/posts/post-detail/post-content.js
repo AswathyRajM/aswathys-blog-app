b0e1c901a237e23b7537d2337640994f087a9890
aswathyrajm <aswathyraj767@gmail.com>
2021-05-26 18:51:04 +0530

    image added from markup

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,15 +1,28 @@
+import Image from "next/image";
 import ReactMarkDown from "react-markdown";
 import PostHeader from "./post-header";
 
 import classes from "./post-content.module.css";
 
 const PostContent = ({ post }) => {
-  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const imagePath = `/images/posts/${post.slug}`;
 
+  const customRenderer = {
+    img(image) {
+      return (
+        <Image
+          src={`${imagePath}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },
+  };
   return (
     <article className={classes.content}>
-      <PostHeader image={imagePath} title={post.title} />
-      <ReactMarkDown>{post.content}</ReactMarkDown>
+      <PostHeader image={`${imagePath}/${post.image}`} title={post.title} />
+      <ReactMarkDown components={customRenderer}>{post.content}</ReactMarkDown>
     </article>
   );
 };
